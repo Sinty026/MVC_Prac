3358393fd7ece0c5f370371577cff2467fbe7820
Sinty026 <115690057+Sinty026@users.noreply.github.com>
2025-08-07 00:22:06 +0930

    Code 1st model ( couldnt get it to work)

---
+++ Models/CodeFirst/ExampleItem.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AOWebApp.Models.CodeFirst
+{
+    public class ExampleItem
+    {
+        [Key]
+        public int itemId { get; set; }
+
+        [Required]
+        [Display(Name = "Item Name")]
+        [StringLength(100, ErrorMessage = "The item name must be less than 100 characters")]
+        public string ItemName { get; set; }
+
+        [Required]
+        [Range(1, 10000)]
+        [Display(Name = "Price")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2")]
+        public decimal ItemPrice { get; set; }
+
+    }
+}
